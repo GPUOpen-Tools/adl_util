5a4b92e717b1be82dcffec13bea94924d5b4ff5c
Chris Hesik <christopher.hesik@amd.com>
2017-09-18 20:37:12 -0400

    Adapt to common-lib-amd-ADL changes

--- ADLUtil.h
+++ ADLUtil.h
@@ -1,5 +1,5 @@
 //==============================================================================
-// Copyright (c) 2011-2016 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2011-2017 Advanced Micro Devices, Inc. All rights reserved.
 /// \author AMD Developer Tools Team
 /// \file
 /// \brief  Interface from Developer Tools to ADL.
@@ -17,7 +17,6 @@
 #include "AMDTMutex.h"
 
 #include "adl_sdk.h"
-// This is from ADL's include directory.
 
 #include <string>
 #include <vector>

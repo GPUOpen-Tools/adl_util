d62c94514326775c83fc129bb89d299c8749ebd1
Chris Hesik <christopher.hesik@amd.com>
2018-12-18 11:31:40 -0500

    Add CMake file for upcoming DevTools release

---
+++ Global-ADLUtil.cmake
@@ -0,0 +1,16 @@
+## Copyright (c) 2018 Advanced Micro Devices, Inc. All rights reserved.
+cmake_minimum_required(VERSION 3.5.1)
+
+## Define ADL Util directory
+set(ADL_UTIL_DIR ${COMMON_DIR}/Src/ADLUtil)
+
+## Include directory
+set(ADDITIONAL_INCLUDE_DIRECTORIES ${ADDITIONAL_INCLUDE_DIRECTORIES}
+                                   ${ADL_UTIL_DIR})
+
+## Declare ADL Util sources
+set(ADL_UTIL_SRC
+    ADLUtil.cpp)
+
+set(ADL_UTIL_HEADERS
+    ADLUtil.h)
